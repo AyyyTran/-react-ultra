58738a8a6e433d7678c04f443ad310992360f8c9
Andy Tran <AyyyTran@gmail.com>
2022-08-26 16:30:28 -0700

    finished website replica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Navbar} from './Components';
+import Home from './Pages/HomePage/Home';
+import Services from './Pages/Services/Services';
+import Products from './Pages/Products/Products';
+import SignUp from './Pages/SignUp/SignUp';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop';
+import {Navbar, Footer} from './Components';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
+      <ScrollToTop />
       <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/Services' element={<Services />} />
+        <Route path='/Products' element={<Products />} />
+        <Route path='/SignUp' element={<SignUp />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
